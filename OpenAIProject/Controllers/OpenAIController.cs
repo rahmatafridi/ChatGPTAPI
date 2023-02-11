8b88a2e96ad7f83da80d90feb93a009769c73b7a
rahmatafrid <khawry900@gmail.com>
2023-02-11 07:12:55 +0500

    upload

---
+++ OpenAIProject/Controllers/OpenAIController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+using OpenAI_API;
+using System.Text;
+
+namespace OpenAIProject.Controllers
+{
+    public class OpenAIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("getanswer")]
+        public IActionResult GetResult([FromBody] string prompt)
+        {
+            //your OpenAI API key
+            string apiKey = "sk-N8NY6OlcxytzRRyhcJ0iXXXXXXXXX";
+            string answer = string.Empty;
+            var openai = new OpenAIAPI(apiKey);
+            CompletionRequest completion = new CompletionRequest();
+            completion.Prompt = prompt;
+            completion.Model = OpenAI_API.Model.DavinciText;
+            completion.MaxTokens = 4000;
+            var result = openai.Completions.CreateCompletionAsync(completion);
+            if (result != null) 
+            {
+                foreach (var item in result.Result.Completions)
+                {
+                    answer = item.Text;
+                }
+                return Ok(answer);
+            }
+            else
+            {
+                return BadRequest("Not found");
+            }
+        }
+    }
+}
